b46e7ad133653e0d8a1c1aaef4e3e8538276fecb
huanghai <10402852@qq.com>
2018-03-15 21:19:16 +0800

    重新整理了课程结构

--- 第0课-公开课/ZhiNengJiQiRen/Test/TestSVM.py
+++
@@ -1,7 +0,0 @@
-from sklearn import datasets, svm
-# 读取数据
-digits = datasets.load_digits()
-# 建立SVM分类器
-clf = svm.SVC(gamma=0.001, C=100.)
-# 使用训练数据对分类器进行训练，它将会返回分类器的某些参数设置
-print(clf.fit(digits.data[:-1], digits.target[:-1]))
--- 第0课-公开课/ZhiNengJiQiRen/Test/why.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-img = cv2.imread('../Images/zhaowei_1.jpg')
-cv2.imshow('w', img)
-cv2.waitKey(0)
