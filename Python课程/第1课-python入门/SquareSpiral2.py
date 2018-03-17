e78281802d8f6a54ba97f37276dc4026f57c1a5d
huanghai <10402852@qq.com>
2018-03-17 20:04:09 +0800

    添加了3个乌龟的小程序

---
+++ Python课程/第1课-python入门/SquareSpiral2.py
@@ -0,0 +1,6 @@
+import turtle
+
+t=turtle.Pen()
+for x in range(1000):
+    t.forward(x)
+    t.left(91)
---
+++ Python课程/第1课-python入门/SquareSpirall.py
@@ -0,0 +1,6 @@
+import turtle
+
+t = turtle.Pen()
+for x in range(100):
+    t.forward(x)
+    t.right(90)
